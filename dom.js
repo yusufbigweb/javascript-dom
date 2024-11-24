60c5ca23e0a6e743425dce26101ca47af973ae08
Yusuf Shaikh <94288456+yusufbigweb@users.noreply.github.com>
2024-11-24 14:42:27 +0000

    What Learn in this Repo - Update

---
+++ dom.js
@@ -0,0 +1,66 @@
+// 1.What is DOM
+// 2.What is node and type of nodes
+// 3.Selecting elements using DOM
+
+    // 1.getElementsById()
+    // 2.getElementsByName()
+    // 3.getElementsByTagName()
+    // 4.getElementsByClassName()
+    // 5.querySelector(), QuerySelectorAll()
+
+
+// 4. Traversing Elements
+
+    // 1.Selecting parent Elements
+    // 2.Selecting Child Elements
+    // 3.Selecting Sibling Elements
+
+
+// 5. Mainpulating HTML Elements
+
+    //  1.createElement()
+    //  2.appendChild()
+    //  3.textContent
+    //  4.innerHTML()
+    //  5.after()
+    //  6.append()
+    //  7.prepend()
+    //  8.insertAdjacentHTML()
+    //  9.replaceChild()
+    //  10.cloneNode()
+    //  11.removeChild()
+    //  12.insertBefore()
+
+
+// 6. Attribute method
+
+        // 1. getAttribute()
+        // 2. setAttribute()
+        // 3. hasAttribute()
+        // 4. removeAttribute()
+
+
+// 7.Mainpulating Element's Style
+
+        //  1.style property
+        //  2.cssText
+        //  3.getComputedStyle()
+        //  4.className Property
+        //  5.classList Property
+
+/* 8.JavaScript Events
+
+        1. Whts is event 
+        2. Event Bubbling & Event Capturing
+        3. Event Handler in HTML Attributes
+        4. DOM Levenl 0 event handlers
+        5. addEventListner()
+        6. removEventListener()
+        7. Event Object
+        8. Different Types of Event
+
+
+
+
+
+*/ 
